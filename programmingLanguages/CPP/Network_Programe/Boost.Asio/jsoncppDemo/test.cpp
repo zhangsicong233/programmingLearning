0f81e57251d5c340d2636ed9e59c9387c9a0aa1a
xunfang <1785803241@qq.com>
2025-12-08 10:47:59 +0800

    Testing if jsoncpp is properly installed and configured.

---
+++ programmingLanguages/CPP/Network_Programe/Boost.Asio/jsoncppDemo/test.cpp
@@ -0,0 +1,19 @@
+#include <json/json.h>
+#include <json/reader.h>
+#include <json/value.h>
+
+#include <iostream>
+
+int main() {
+  Json::Value root;
+  root["id"] = 1001;
+  root["data"] = "hello world";
+  std::string request = root.toStyledString();
+  std::cout << "request is " << request << std::endl;
+
+  Json::Value root2;
+  Json::Reader reader;
+  reader.parse(request, root2);
+  std::cout << "msg id is " << root2["id"] << " msg is " << root2["data"]
+            << std::endl;
+}
