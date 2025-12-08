7106f8023efc53fd6c3f96653051223fa2619171
xunfang <1785803241@qq.com>
2025-12-08 19:13:54 +0800

    Logical layer construction.

---
+++ programmingLanguages/CPP/Network_Programe/Boost.Asio/逻辑层构建/MsgNode.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <cstring>
+#include <iostream>
+
+class MsgNode {
+ public:
+  MsgNode(short max_len);
+
+  void Clear();
+
+  ~MsgNode();
+
+  short _cur_len;
+  short _total_len;
+  char* _data;
+};
+
+class RecvNode : public MsgNode {
+ public:
+  RecvNode(short max_len, short msg_id);
+
+ private:
+  short _msg_id;
+};
+
+class SendNode : public MsgNode {
+ public:
+  SendNode(const char* msg, short max_len, short msg_id);
+
+ private:
+  short _msg_id;
+};
---
+++ programmingLanguages/CPP/Network_Programe/Boost.Asio/逻辑层构建/const.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define HEAD_TOTAL_LEN 4
+#define HEAD_ID_LEN 2
+#define HEAD_DATA_LEN 2
+#define MAX_LENGTH 1024 * 2
+#define MAX_RECVQUE 10000
+#define MAX_SENDQUE 1000
